2fc470d60c2e4bca8fa241a3a9ff4786c665ffac
Troy Warr <troy3000@gmail.com>
2015-03-14 01:02:10 -0500

    change blink interval

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var Gpio = require('onoff').Gpio,
     led = new Gpio(17, 'out'),
     iv = setInterval(function() {
       led.writeSync(led.readSync() === 0 ? 1 : 0);
-    }, 500);
+    }, 50);
 
-// stop blinking the LED and turn it off after 5 seconds
+// stop blinking the LED and turn it off after 1 second
 setTimeout(function() {
   clearInterval(iv); // stop blinking
   led.writeSync(0); // turn LED off
   led.unexport(); // unexport GPIO and free resources
-}, 5000);
+}, 1000);
